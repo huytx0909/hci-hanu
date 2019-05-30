f55cc5e18780ee0d06adc3839c4997b972aae70b
Huytx <huytx0909@gmail.com>
2019-05-30 16:38:50 +0700

    add files' function stubs

---
+++ src/js/file/script.js
@@ -0,0 +1,19 @@
+function folderClicked(folderName) {
+    alert("folder clicked" + folderName);
+}
+
+function folderDelete(folderName) {
+    alert("delete folder: " + folderName + "?");
+}
+
+function updateFolder(folderName) {
+    alert("update folder: " + folderName + "?");
+}
+
+function createFolder(folderName) {
+    alert("created folder: " + folderName);
+}
+
+function search(name) {
+    alert(name);
+}
