922cba964a4ec8d904523da2bdaf723deff3b767
Huytx <huytx0909@gmail.com>
2019-06-01 01:21:03 +0700

    delete file function

--- src/js/file/script.js
+++ src/js/file/script.js
@@ -2,8 +2,12 @@ function folderClicked(folderName) {
     alert("folder clicked" + folderName);
 }
 
-function folderDelete(folderName) {
-    alert("delete folder: " + folderName + "?");
+function folderDelete() {
+    homeClick();
+    var myClass = document.getElementsByClassName("alert");
+    for (var i = 0; i < myClass.length; i++) {
+        myClass[i].style.visibility = 'visible';
+      }
 }
 
 function updateFolder(folderName) {
@@ -16,4 +20,26 @@ function createFolder(folderName) {
 
 function search(name) {
     alert(name);
+}
+
+function confirmAction() {
+    var myClass = document.getElementsByClassName("alert");
+    for (var i = 0; i < myClass.length; i++) {
+        myClass[i].style.visibility = 'hidden';;
+      }
+    alert("Action performed successfully");
+}
+
+function cancelAction() {
+    var myClass = document.getElementsByClassName("alert");
+    for (var i = 0; i < myClass.length; i++) {
+        myClass[i].style.visibility = 'hidden';
+      }
+}
+
+function homeClick() {
+	var elmnt = document.getElementById("alert-confirm");
+	elmnt.scrollIntoView({
+	 behavior: 'smooth', block: "start"}
+	);
 }
