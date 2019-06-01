e3bbdc65e48ca925c2bdea368b60739b6b74d5d5
Huytx <huytx0909@gmail.com>
2019-06-01 16:05:54 +0700

    read,edit, delete, create and back button

--- src/js/file/script.js
+++ src/js/file/script.js
@@ -10,17 +10,41 @@ function getFolderName() {
 function folderDelete() {
     homeClick();
     var myClass = document.getElementsByClassName("alert");
-    for (var i = 0; i < myClass.length; i++) { 
+    for (var i = 0; i < myClass.length; i++) {
         myClass[i].style.visibility = 'visible';
-      }
+    }
 }
 
-function updateFolder(folderName) {
-    alert("update folder: " + folderName + "?");
+function updateFolder() {
+    bootbox.prompt("Rename file to: ", function (result) {
+        if (result) {
+            bootbox.alert({
+                message: "Rename!",
+                size: 'large'
+            });
+        } else {
+            bootbox.alert({
+                message: "Canceled or Invalid name!",
+                size: 'large'
+            })
+        };
+    });
 }
 
 function createFolder(folderName) {
-    alert("created folder: " + folderName);
+    bootbox.prompt("Enter the new name:", function (result) {
+        if (result) {
+            bootbox.alert({
+                message: "Created!",
+                size: 'large'
+            });
+        } else {
+            bootbox.alert({
+                message: "Canceled or Invalid name!",
+                size: 'large'
+            })
+        };
+    });
 }
 
 function search(name) {
@@ -31,22 +55,26 @@ function confirmAction() {
     var myClass = document.getElementsByClassName("alert");
     for (var i = 0; i < myClass.length; i++) {
         myClass[i].style.visibility = 'hidden';;
-      }
-    alert("Action performed successfully");
+    }
+    bootbox.alert({
+        message: "Actions have been saved!",
+        size: 'large'
+    });
 }
 
 function cancelAction() {
     var myClass = document.getElementsByClassName("alert");
     for (var i = 0; i < myClass.length; i++) {
         myClass[i].style.visibility = 'hidden';
-      }
+    }
 }
 
 function homeClick() {
-	var elmnt = document.getElementById("alert-confirm");
-	elmnt.scrollIntoView({
-	 behavior: 'smooth', block: "start"}
-	);
+    var elmnt = document.getElementById("alert-confirm");
+    elmnt.scrollIntoView({
+        behavior: 'smooth', block: "start"
+    }
+    );
 }
 
 function folderDownload() {
